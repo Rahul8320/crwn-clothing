6dc4f29862a763d4e4bf9ce834051e180195b0ce
Rahul Dey <rahuldey832000@gmail.com>
2024-03-06 09:57:12 +0530

    Add sass into project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import "./categories.style.scss";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      image: "",
+      title: "Hats",
+    },
+    {
+      id: 2,
+      image: "",
+      title: "Jeans",
+    },
+    {
+      id: 3,
+      image: "",
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      image: "",
+      title: "Women",
+    },
+    {
+      id: 5,
+      image: "",
+      title: "Men",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Saving this dying World</h1>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <button>Shop Now</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
